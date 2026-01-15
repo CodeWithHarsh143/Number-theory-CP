34e46478212ba0913155495e5d3890c74d8a93f9
HarshBaghel <shivambaghel1467@gmail.com>
2026-01-15 11:29:53 +0530

    Add files via upload

---
+++ lcm_ecd.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long gcd(long long a , long long b){
+    while(b!=0){
+        long long r = a%b;
+        a=b;
+        b=r;
+    }
+    return a;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    /*
+    we know lcm(a,b)*gcd(a,b) = a*b
+    --->lcm(a,b) = a*b/gcd(a,b)    
+    */
+
+    long long a,b,lcm;
+    cin >> a >> b;
+    lcm = a*b/gcd(a,b);
+    cout << lcm << endl;
+    
+
+    return 0;
+}
